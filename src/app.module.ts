67e3f295a75c1ed99d0a27e398bc23f149e0dcbf
MaxR522 <ranjamario@gmail.com>
2022-09-11 09:33:03 +0300

    👌 IMPROVE: config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb://blog:myblog123123@0.0.0.0:27017/myBlogDb',
+      `mongodb://blog:myblog123123@$0.0.0.0:27017/myBlogDb`,
     ),
+    ConfigModule.forRoot({ envFilePath: './.env', isGlobal: true }),
   ],
   controllers: [AppController],
   providers: [AppService],
